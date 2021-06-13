16c5f8acf3552aec7d62476e0b0a5a4d6993faa5
Sigma88 <sigmamodexpansions@outlook.com>
2021-06-13 11:37:02 +0200

    clear focus when saving

--- SigmaAddOnManager/Frames/Buttons/SaveProfile.lua
+++ SigmaAddOnManager/Frames/Buttons/SaveProfile.lua
@@ -7,5 +7,11 @@ SAOM.SaveProfile:SetWidth(85);
 SAOM.SaveProfile:SetPoint("LEFT", AddonListDisableAllButton, "RIGHT", 1, 0);
 -- Set the text
 SAOM.SaveProfile:SetText(SAOM.L["SAVE"]);
+
 -- Set Scripts
-SAOM.SaveProfile:SetScript("OnClick", function() StaticPopup_Show("SigmaAddOnManager_SaveProfile"); end);
+function SAOM.SaveProfile_OnClick()
+	-- Clear the SearchBox Focus
+    SAOM.SearchBox:ClearFocus();
+	StaticPopup_Show("SigmaAddOnManager_SaveProfile");
+end
+SAOM.SaveProfile:SetScript("OnClick", SAOM.SaveProfile_OnClick);
