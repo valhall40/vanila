18ac0d020a791eb75e24d45b411dbc9e7e025518
Sigma88 <sigmamodexpansions@outlook.com>
2021-07-06 22:10:30 +0200

    replace toggle

--- SigmaAddOnManager/Core/ReplaceFunctions.lua
+++ SigmaAddOnManager/Core/ReplaceFunctions.lua
@@ -15,9 +15,6 @@ function SAOM.GetAddOnInfo(index)
 	end
 end
 
-SAOM.baseGetAddOnInfo = GetAddOnInfo;
-GetAddOnInfo = SAOM.GetAddOnInfo;
-
 function SAOM.GetAddOnDependencies(index)
 	local number = tonumber(index);
 	if number then
@@ -31,14 +28,28 @@ function SAOM.GetAddOnDependencies(index)
 	end
 end
 
-SAOM.baseGetAddOnDependencies = GetAddOnDependencies;
-GetAddOnDependencies = SAOM.GetAddOnDependencies;
-
 function SAOM.AddonTooltip_Update(owner)
 	if owner then
 		return SAOM.baseAddonTooltip_Update(owner);
 	end
 end
 
-SAOM.baseAddonTooltip_Update = AddonTooltip_Update;
-AddonTooltip_Update = SAOM.AddonTooltip_Update;
+SAOM.Frame = CreateFrame("Frame");
+
+function SAOM.Frame:OnEvent(event, arg1, ...)
+	if event == "ADDON_LOADED" and arg1 == "SigmaAddOnManager" then
+		if SigmaAddOnManager.ReplaceBlizzFunctions ~= false then
+			SAOM.baseGetAddOnInfo = GetAddOnInfo;
+			GetAddOnInfo = SAOM.GetAddOnInfo;
+
+			SAOM.baseGetAddOnDependencies = GetAddOnDependencies;
+			GetAddOnDependencies = SAOM.GetAddOnDependencies;
+
+			SAOM.baseAddonTooltip_Update = AddonTooltip_Update;
+			AddonTooltip_Update = SAOM.AddonTooltip_Update;
+		end
+	end
+end
+ 
+SAOM.Frame:RegisterEvent("ADDON_LOADED");
+SAOM.Frame:SetScript("OnEvent", SAOM.Frame.OnEvent);
